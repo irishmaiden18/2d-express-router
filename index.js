1751fdf908b779058ce692f1dc1435f81913cab9
irishmaiden18 <laurapohl18@gmail.com>
2026-03-13 12:07:16 -0400

    basic set up

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+// get set up
+// npm init -y
+// npm install express morgan
+
 /*
     1. Import express & Morgan, set up app variable
 */
+const express = require("express")
+const logger = require("morgan")
+
+const app = express()
 
 /*
     2. Set up middleware
 */
+
+// morgan
+app.use(logger("dev"))
+
+// formats our express body
+app.use(express.json())
 
 /*
     3. Import Router files
@@ -17,3 +31,9 @@
 /*
     5. Set up the port and begin listening
 */
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on Port: ${PORT}`)
+})
